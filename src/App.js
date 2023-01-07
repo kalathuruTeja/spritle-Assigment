00d050f6dc51685bd27e0745e56e377510811c83
kalathurutejaa <kalathuruteja7883@gmail.com>
2023-01-07 10:22:51 +0530

    assigment

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+
+import SignUp from './components/SignUp';
+import Login from "./components/Login";
+import Game from "./components/Game";
+import LoginSuccess from "./components/LoginSuccess";
+
+
+
+
+function App() {
+
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route exact path="/" element={<Login/>}/>
+    <Route exact path="/signUp" element={<SignUp/>}/>
+    <Route exact path="/game" element={<Game/>}/>
+    <Route exact path="/log" element={<LoginSuccess/>}/> 
+  </Routes>
+  </BrowserRouter>
+  );
+  }
+export default App
